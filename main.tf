e8a211bc8e39eb54e253f3b200584c2a236f4a83
vishwabk31 <vishwabkumar@gmail.com>
2025-09-20 20:32:48 +0530

    true

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-test vb kumar
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.45.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  # not done
+}
